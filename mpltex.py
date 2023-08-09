29717cbe02758a2d440848b928a90997ee46852e
johashei <j.s.heines@fys.uio.no>
2023-08-09 10:21:35 +0200

    pass first test

--- mpltex.py
+++ mpltex.py
@@ -5,4 +5,20 @@ from beartype import beartype
 
 @beartype
 def extract_text(fig: plt.Figure):
-    pass
+    return set([t for t in get_text_decendents(fig)
+                if t.get_visible() and t.get_text() != ''])
+
+@beartype
+def get_text_decendents(artist: mpl.artist.Artist):
+    stack = [iter(artist.get_children())]
+    print(stack)
+    while stack:
+        try:
+            child = next(stack[-1])
+            #print(child)
+            if isinstance(child, mpl.text.Text):
+                yield child
+            else:
+                stack.append(iter(child.get_children()))
+        except StopIteration:
+            stack.pop()
--- tests.py
+++ tests.py
@@ -5,14 +5,23 @@ import matplotlib.pyplot as plt
 
 @pytest.fixture
 def make_simple_figure():
+    """Make a figure with a single subplot and unique text objects."""
     fig, ax = plt.subplots()
-    ax.plot([0,1], [0,1])
+    ax.plot([0,1], [0,2])
     ax.set_xlabel("x axis label")
     ax.set_ylabel("y axis label")
     ax.set_title("axis title")
-    return fig
+    ax.set_xticks([0, 1, 2], ['none', 'I', 'II'])
+    ax.set_yticks([0, 1/3, 2/3, 1], ['0', '1/3', '2/3', '1'])
+    visible_text_objects = set(
+        [ax.title, ax.xaxis.label, ax.yaxis.label]
+        + ax.get_xticklabels()
+        + ax.get_yticklabels()
+        )
+    return fig, visible_text_objects
+
 
 def test_get_text(make_simple_figure):
-    expected = set(("x axis label", "y axis label", "axis title"))
-    result = mpltex.extract_text(make_simple_figure)
+    result = mpltex.extract_text(make_simple_figure[0])
+    expected = make_simple_figure[1]
     assert result == expected
