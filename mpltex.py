dcdb92bd2aef6d239577f975f8c4ed8bb5aded88
johashei <j.s.heines@fys.uio.no>
2023-08-15 10:12:30 +0200

    Remove text with clip_on=True that is completely outside the corresponding axes, i.e. outside both x and y range.

--- mpltex.py
+++ mpltex.py
@@ -20,12 +20,11 @@ def save(fig: plt.Figure, /, filename: str):
 def write_tex(output: LaTeXinput, fig, *, graphics):
     output.includegraphics(graphics)
     for element in extract_text(fig):
-        xy = get_position_in_figure(fig, element)
-        draw_anchors(fig, xy) # useful for checking positioning
+        draw_anchors(fig, element.get_position_in_figure()) # useful for checking positioning
         output.add_text(
             element.get_text(),
-            position=xy,
-            anchor=get_tikz_anchor(element),
+            position=element.get_position_in_figure(),
+            anchor=element.get_tikz_anchor(),
             rotation=element.get_rotation())
 
 
@@ -35,29 +34,60 @@ class FigureText:
             self,
             text: mpl.text.Text,
             fig: plt.Figure,
-            ax: plt.Axes | None = None):
+            ax: plt.Axes | None):
         """Constructor for the FigureText class."""
         self._mpl_text = text
         self._figure_transform = fig.transFigure
         self._ax = ax
 
-    @property
-    def text(self):
+    # I use getters and setters because the code behind them can be
+    # complicated, take time, or fail. These are thing a user probably
+    # doesn't extect attribute assignments to do. I may change my mind
+    # about this in the future.
+
+    def __str__(self):
+        return (
+            f"FigureText({self._mpl_text}, position={self._figure_xy}, "
+            f"visible={self._visible}, tikz_anchor={self._tikz_anchor})")
+
+    def get_text(self):
         return self._mpl_text.get_text()
 
-    @property
-    def rotation(self):
+    def get_tikz_anchor(self):
+        return self._tikz_anchor
+
+    def get_position_in_figure(self):
+        return self._figure_xy
+
+    def get_rotation(self):
         return self._mpl_text.get_rotation()
 
-    @cached_property
-    def position_in_figure(self):
-        display_xy = self._mpl_text.get_transform().transform(
-            self._mpl_text.get_position())
-        figure_xy = self.figure_transform.inverted().transform(display_xy)
-        return figure_xy
+    def get_visible(self):
+        return self._visible
+
+    def get_color(self):
+        return self._mpl_text.get_color()
+
+    def set_color(self, value, /):
+        self._mpl_text.set_color(value)
 
     @cached_property
-    def tikz_anchor(self):
+    def _display_xy(self):
+        return self._mpl_text.get_transform().transform(
+            self._mpl_text.get_position())
+
+    @cached_property
+    def _figure_xy(self):
+        return self._figure_transform.inverted().transform(self._display_xy)
+
+    @cached_property
+    def _axes_xy(self):
+        if self._ax is None:
+            raise ValueError("This text does not belong to any Axes")
+        return self._ax.transAxes.inverted().transform(self._display_xy)
+
+    @cached_property
+    def _tikz_anchor(self):
         anchor_by_va = {
             'bottom': 'south',
             'top': 'north',
@@ -77,7 +107,7 @@ class FigureText:
         return anchor
 
     @cached_property
-    def visible(self):
+    def _visible(self):
         if not self._mpl_text.get_visible():
             return False
         elif self._ax is None or not self._mpl_text.get_clip_on():
@@ -86,10 +116,8 @@ class FigureText:
             return self._is_inside_ax()
 
     def _is_inside_ax(self):
-        x, y = self._mpl_text.get_position()
-        xmin, xmax = self.ax.get_xlim()
-        ymin, ymax = self.ax.get_ylim()
-        if (xmin < x < xmax) and (ymin < y < ymax):
+        x, y = self._axes_xy
+        if (0 <= x <= 1) or (0 <= y <= 1):
             return True
         else:
             return False
@@ -113,25 +141,27 @@ def remove_transparent(text_set: set, /):
 def make_all_transparent(fig: plt.Figure, /):
     removed_colors = {}
     for text in get_text_decendents(fig):
-        removed_colors[text] = text.get_color()
+        removed_colors[text._mpl_text] = text.get_color()
         text.set_color("none")  # avoids messing with the whitespace
     return removed_colors
 
 @beartype
 def restore_colors(fig: plt.Figure, colors: dict):
-    pass
     for text in get_text_decendents(fig):
-        text.set_color(colors[text])
+        text.set_color(colors[text._mpl_text])
 
 @beartype
-def get_text_decendents(artist: mpl.artist.Artist, /):
-    stack = [iter(artist.get_children())]
+def get_text_decendents(fig: plt.Figure, /):
+    stack = [iter(fig.get_children())]
+    current_ax = None
     while stack:
         try:
             child = next(stack[-1])
             if isinstance(child, mpl.text.Text):
-                yield child
+                yield FigureText(text=child, fig=fig, ax=current_ax)
             else:
+                if isinstance(child, plt.Axes):
+                    current_ax = child
                 stack.append(iter(child.get_children()))
         except StopIteration:
             stack.pop()
@@ -141,4 +171,18 @@ def draw_anchors(fig, figure_xy):
     ax.plot(figure_xy[0], figure_xy[1], '+r', clip_on=False, transform=fig.transFigure,
             zorder=20)
 
-
+def print_family_tree(mpl_object):
+    stack = [iter(mpl_object.get_children())]
+    print(stack)
+    indent = ""
+    while stack:
+        try:
+            child = next(stack[-1])
+            print(f"{indent}{child}")
+            stack.append(iter(child.get_children()))
+            indent = indent[:-2]
+            indent += "  |- "
+        except StopIteration:
+            indent = indent[:-5]
+            indent += "- "
+            stack.pop()
