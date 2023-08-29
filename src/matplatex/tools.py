4efc263bf6b270c7afbf78c09665e800b8753524
johashei <j.s.heines@fys.uio.no>
2023-08-29 12:09:12 +0200

    changed getters to properties or attributes

--- src/matplatex/tools.py
+++ src/matplatex/tools.py
@@ -1,4 +1,5 @@
 from functools import cached_property
+from typing import Iterator
 
 import matplotlib.pyplot as plt
 from beartype import beartype
@@ -8,12 +9,12 @@ from .latex_input import LaTeXinput
 def write_tex(output: LaTeXinput, fig, *, graphics):
     output.includegraphics(graphics)
     for element in extract_text(fig):
-        draw_anchors(fig, element.get_position_in_figure()) # useful for checking positioning
+        draw_anchors(fig, element.position_in_figure) # useful for checking positioning
         output.add_text(
-            element.get_text(),
-            position=element.get_position_in_figure(),
-            anchor=element.get_tikz_anchor(),
-            rotation=element.get_rotation())
+            element.text,
+            position=element.position_in_figure,
+            anchor=element.tikz_anchor,
+            rotation=element.rotation)
 
 class FigureText:
     """Contain text and its tikz properties."""
@@ -23,58 +24,37 @@ class FigureText:
             fig: plt.Figure,
             ax: plt.Axes | None):
         """Constructor for the FigureText class."""
-        self._mpl_text = text
+        self.mpl_text = text
         self._figure_transform = fig.transFigure
         self._ax = ax
 
-    # I use getters and setters because the code behind them can be
-    # complicated, take time, or fail. These are thing a user probably
-    # doesn't extect attribute assignments to do. I may change my mind
-    # about this in the future.
-
     def __str__(self):
         return (
-            f"FigureText({self._mpl_text}, position={self._figure_xy}, "
+            f"FigureText({self.mpl_text}, position={self._figure_xy}, "
             f"visible={self._visible}, tikz_anchor={self._tikz_anchor})")
 
-    def get_text(self):
-        return self._mpl_text.get_text()
+    @property
+    def text(self) -> str:
+        return self.mpl_text.get_text()
 
-    def get_tikz_anchor(self):
-        return self._tikz_anchor
-
-    def get_position_in_figure(self):
+    @property  # Disconnect from attribute so I can change how it works.
+    def position_in_figure(self) -> tuple[float]:
         return self._figure_xy
 
-    def get_rotation(self):
-        return self._mpl_text.get_rotation()
+    @property
+    def rotation(self) -> float:
+        return self.mpl_text.get_rotation()
 
-    def get_visible(self):
-        return self._visible
+    @property
+    def color(self):
+        return self.mpl_text.get_color()
 
-    def get_color(self):
-        return self._mpl_text.get_color()
-
-    def set_color(self, value, /):
-        self._mpl_text.set_color(value)
-
-    @cached_property
-    def _display_xy(self):
-        return self._mpl_text.get_transform().transform(
-            self._mpl_text.get_position())
+    @color.setter
+    def color(self, value, /):
+        self.mpl_text.set_color(value)
 
     @cached_property
-    def _figure_xy(self):
-        return self._figure_transform.inverted().transform(self._display_xy)
-
-    @cached_property
-    def _axes_xy(self):
-        if self._ax is None:
-            raise ValueError("This text does not belong to any Axes")
-        return self._ax.transAxes.inverted().transform(self._display_xy)
-
-    @cached_property
-    def _tikz_anchor(self):
+    def tikz_anchor(self) -> str:
         anchor_by_va = {
             'bottom': 'south',
             'top': 'north',
@@ -87,20 +67,35 @@ class FigureText:
             'left': 'west',
             'center': ''
             }
-        anchor = (f"{anchor_by_va[self._mpl_text.get_va()]} "
-                  f"{anchor_by_ha[self._mpl_text.get_ha()]}")
+        anchor = (f"{anchor_by_va[self.mpl_text.get_va()]} "
+                  f"{anchor_by_ha[self.mpl_text.get_ha()]}")
         if anchor == ' ':
             anchor = 'center'
         return anchor
 
     @cached_property
-    def _visible(self):
-        if not self._mpl_text.get_visible():
+    def visible(self) -> bool:
+        if not self.mpl_text.get_visible():
             return False
-        elif self._ax is None or not self._mpl_text.get_clip_on():
+        elif self._ax is None or not self.mpl_text.get_clip_on():
             return True
         else:
             return self._is_inside_ax()
+
+    @cached_property
+    def _display_xy(self):
+        return self.mpl_text.get_transform().transform(
+            self.mpl_text.get_position())
+
+    @cached_property
+    def _figure_xy(self):
+        return self._figure_transform.inverted().transform(self._display_xy)
+
+    @cached_property
+    def _axes_xy(self):
+        if self._ax is None:
+            raise ValueError("This text does not belong to any Axes")
+        return self._ax.transAxes.inverted().transform(self._display_xy)
 
     def _is_inside_ax(self):
         x, y = self._axes_xy
@@ -116,29 +111,29 @@ def extract_text(fig: plt.Figure, /):
         set(get_text_decendents(fig)))))
 
 def remove_invisible(text_set: set, /):
-    return {element for element in text_set if element.get_visible()}
+    return {element for element in text_set if element.visible}
 
 def remove_empty(text_set: set, /):
-    return {element for element in text_set if element.get_text() != ''}
+    return {element for element in text_set if element.text != ''}
 
 def remove_transparent(text_set: set, /):
-    return {element for element in text_set if element.get_color() != 'none'}
+    return {element for element in text_set if element.color != 'none'}
 
 @beartype
 def make_all_transparent(fig: plt.Figure, /):
     removed_colors = {}
     for text in get_text_decendents(fig):
-        removed_colors[text._mpl_text] = text.get_color()
-        text.set_color("none")  # avoids messing with the whitespace
+        removed_colors[text.mpl_text] = text.color
+        text.color = "none"  # avoids messing with the whitespace
     return removed_colors
 
 @beartype
 def restore_colors(fig: plt.Figure, colors: dict):
     for text in get_text_decendents(fig):
-        text.set_color(colors[text._mpl_text])
+        text.color = colors[text.mpl_text]
 
 @beartype
-def get_text_decendents(fig: plt.Figure, /):
+def get_text_decendents(fig: plt.Figure, /) -> Iterator[FigureText]:
     stack = [iter(fig.get_children())]
     current_ax = None
     while stack:
