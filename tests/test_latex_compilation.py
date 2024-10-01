339328c35bc9d3016f3a7cbc91fbf1ed884e8148
J.S.Heines <j.s.heines@protonmail.com>
2024-10-01 17:02:49 +0200

    add support for tikz externalization

--- tests/test_latex_compilation.py
+++ tests/test_latex_compilation.py
@@ -6,22 +6,7 @@ import pytest
 
 from matplatex import save
 
-LATEX_MWE = r"""
-\documentclass{article}
-
-\usepackage{graphicx}
-\usepackage{tikz}
-
-\newlength{\figurewidth}
-\newlength{\matplatextmp}
-
-\begin{document}
-
-\setlength{\figurewidth}{\linewidth}
-\input{figure.pdf_tex}
-
-\end{document}
-"""
+from .latex_test_code import MWE, TIKZEXTERNALIZE
 
 @pytest.fixture
 def figure():
@@ -40,16 +25,27 @@ def figure():
     ax2.plot([0, 3, 4, 7], [15, 11, 7, 3], '--')
     return fig
 
-@pytest.fixture
-def latex_source(figure, tmp_path):
-    latex_path = tmp_path / 'document.tex'
+@pytest.fixture(params=[MWE, TIKZEXTERNALIZE])
+def latex_source(request, figure, tmp_path):
+    latex_path = tmp_path / 'document.tex' # Must match the \pgfrealjobname.
     figure_path = tmp_path / 'figure'
     # Regenerate the files each time so changes are applied.
-    latex_path.write_text(LATEX_MWE, encoding='utf-8')
-    save(figure, str(figure_path), verbose=2)
-    return {'dir': tmp_path, 'file': latex_path.name}
+    externalize = request.param == TIKZEXTERNALIZE
+    latex_path.write_text(request.param, encoding='utf-8')
+    save(figure, str(figure_path), externalize=externalize, verbose=2)
+    return {'dir': tmp_path,
+            'file': latex_path.name,
+            'ext': externalize
+            }
 
 def test_compilation(latex_source):
+    # Several asserts here because they are ordered.
+    if latex_source['ext']: # requires first compiling figures
+        figure_compilation = run(
+            ['pdflatex', '--jobname=figure_xt', latex_source['file']],
+            cwd=latex_source['dir']
+            )
+        assert figure_compilation.returncode == 0
     # compile with pdflatex
     compilation = run(
         ['pdflatex', latex_source['file']],
