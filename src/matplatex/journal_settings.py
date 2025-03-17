61e3f2d0f1a0ad9398c575e7faaa9b46659bef5b
J.S.Heines <j.s.heines@protonmail.com>
2025-03-17 16:44:54 +0100

    make journals uppercase

--- src/matplatex/journal_settings.py
+++ src/matplatex/journal_settings.py
@@ -5,13 +5,13 @@ from dataclasses import dataclass
 _inches_per_mm = 0.03937
 
 @dataclass
-class epj:
+class EPJ:
     column_width = 88 * _inches_per_mm
     full_width = 180 * _inches_per_mm
     font_size = 10
 
 @dataclass
-class prc:
+class PRC:
     column_width = 3.36
     full_width = 6.75
     font_size = 10
